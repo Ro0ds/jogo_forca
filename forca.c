0adda9f7604a9cb9a5e5b00b39ac00f2d9212bba
Rodrigo Gabriel <digo_gabriel97@yahoo.com.br>
2022-12-29 02:07:59 -0300

    Implementado config para guardar chutes e printar resultado

--- forca.c
+++ forca.c
@@ -4,15 +4,41 @@
 
 int main(){
     char palavrasecreta[20];
+    char chutes[26];
+    int tentativas = 0;
     int acertou = 0;
     int enforcou = 0;
 
+
     sprintf(palavrasecreta, "MELANCIA");
 
-    do{
+    do {
+        for(int i = 0; i < strlen(palavrasecreta); i++){
+            int achou = 0;
+
+            for(int j = 0; j < tentativas; j++){
+                if(chutes[j] == palavrasecreta[i]){
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if (achou){
+                printf("%c", palavrasecreta[i]);
+            }
+            else{
+                printf("_ ");
+            }
+        }
+
+        printf("\n");
+
         char chute;
         printf("Qual letra? ");
         scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
 
         for(int i = 0; i < strlen(palavrasecreta); i++){
             if(palavrasecreta[i] == chute){
